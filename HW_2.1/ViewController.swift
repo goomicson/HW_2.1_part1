f1d03521774a07faeae489fa7341ebe3d5a60f67
Sergei <Goomicson@gmail.com>
2021-10-19 20:25:39 +0400

    First part of the first homework for Swiftbook

---
+++ HW_2.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW_2.1
+//
+//  Created by Сергей on 19.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
